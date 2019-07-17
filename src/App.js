20de0a57bdf1c9339f4edeed15a36aca2d6e2ddd
JasonYoo1 <jyoojs@gmail.com>
2019-07-17 13:03:47 -0700

    set axios in html and import. created Header.js and Display.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import Header from './Header'
+import Images from './Display'
+
 
 function App() {
+
+  
   return (
     <div className="App">
+      <Header/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+
     </div>
   );
 }
