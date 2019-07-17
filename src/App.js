7a24c49e975eeb0f658c2801c10044477bd8c08e
JasonYoo1 <jyoojs@gmail.com>
2019-07-17 14:59:47 -0700

    created states from object, connected keys to variables

--- src/App.js
+++ src/App.js
@@ -2,22 +2,45 @@ import React,{ useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import Header from './Header'
-import Images from './Display'
+// import Images from './Display'
 
 
 function App() {
 
-  
-  return (
-    <div className="App">
-      <Header/>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+  //creating states from the objects given in NASA website
+  const [date, setDate] = useState('');
+  const [explanation, setExplanation] = useState('');
+  const [media, setMedia] = useState('');
+  const [version, setVersion] = useState('');
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState ('');
 
-    </div>
-  );
+    useEffect( () => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo ')
+      .then((res)=>{
+        console.log(res.data);
+        const results = res.data;
+        // console.log(results)
+        setDate (results.date);
+        setExplanation(results.explanation);
+        setMedia (results.media_type);
+        setVersion (results.service_version);
+        setTitle (results.title);
+        setUrl (results.url);})
+      })
+      return (
+        <div className="App">
+          <Header/>
+          <p>
+            Read through the instructions in the README.md file to build your NASA
+            app! Have fun 🚀!
+          </p>
+          <p>{title}</p>
+          <p>{date}</p>
+          <p>{explanation}</p>
+          <p>{version}</p>
+          <a href={url}>Click Me</a>
+        </div>)
 }
 
 export default App;
--- src/Display.js
+++ src/Display.js
@@ -2,18 +2,3 @@ import React,{ useState, useEffect } from "react";
 import axios from 'axios';
 
 
-const Images = (props)=>{
-    const [state, setState] = useState(true);
-    useEffect( () => { console.log('This is my State:', state)});
-    axios.get('https://api.nasa.gov/planetary/apod')
-  .then(res=>{
-    console.log(res)
-  })
-
-    return <div>
-
-    </div>
-}
-
-export default Images
-
